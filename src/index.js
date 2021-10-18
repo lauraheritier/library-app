f58f15a6c51c2a93e87365aefa02836c968076db
Laura Heritier <laura.heritier@gmail.com>
2021-10-18 17:24:16 -0300

    Auth0 added to app

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(
-  <React.StrictMode>
+ReactDOM.render( 
+
+< Auth0Provider domain = "dev-floe0emw.us.auth0.com"
+    clientId = "67OelSk6X6E5baaNRU16TBide7PAk9ms"
+    redirectUri = { window.location.origin } >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+    </Auth0Provider>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
