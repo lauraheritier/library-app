538a069b6cd888b05c7abf6bf0dcbf14c14ffd68
Laura Heritier <laura.heritier@gmail.com>
2021-10-18 18:19:32 -0300

    replaced MongoCompass with native mongodb

--- src/server/config/db.config.js
+++ src/server/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-     url: "mongodb+srv://laurah:caramelo@cluster0.xjvmo.mongodb.net/library?authSource=admin&replicaSet=atlas-d39ort-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+     
+     url: "mongodb://localhost:27017/libraryApp"
 };
